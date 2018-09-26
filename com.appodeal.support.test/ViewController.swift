06f41d2641c242559ce89296632cf8525f9dbbf7
Andrew Zeniukevich <del.exe@gmail.com>
2018-09-26 19:44:37 +0200

    Initial Commit

---
+++ com.appodeal.support.test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  com.appodeal.support.test
+//
+//  Created by andrei zeniukevich on 26/09/2018.
+//  Copyright © 2018 andrei zeniukevich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
